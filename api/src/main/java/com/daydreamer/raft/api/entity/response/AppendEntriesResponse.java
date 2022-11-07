361978de3a0dd6c7f38baabd36207167562a12d3
Daydreamer-ia <2296032269@qq.com>
2022-11-07 20:11:15 +0800

    fix(log): remove unused field in response

--- api/src/main/java/com/daydreamer/raft/api/entity/response/AppendEntriesResponse.java
+++ api/src/main/java/com/daydreamer/raft/api/entity/response/AppendEntriesResponse.java
@@ -12,20 +12,8 @@ public class AppendEntriesResponse extends Response {
      */
     private boolean accepted;
     
-    /**
-     * current follower term
-     */
-    private int followerTerm;
-    
-    /**
-     * current follower last committed log id
-     */
-    private long followerLogId;
-    
-    public AppendEntriesResponse(boolean accepted, int followerTerm, long followerLogId) {
+    public AppendEntriesResponse(boolean accepted) {
         this.accepted = accepted;
-        this.followerLogId = followerLogId;
-        this.followerTerm = followerTerm;
     }
     
     public boolean isAccepted() {
@@ -34,21 +22,5 @@ public class AppendEntriesResponse extends Response {
     
     public void setAccepted(boolean accepted) {
         this.accepted = accepted;
-    }
-    
-    public int getFollowerTerm() {
-        return followerTerm;
-    }
-    
-    public void setFollowerTerm(int followerTerm) {
-        this.followerTerm = followerTerm;
-    }
-    
-    public long getFollowerLogId() {
-        return followerLogId;
-    }
-    
-    public void setFollowerLogId(long followerLogId) {
-        this.followerLogId = followerLogId;
     }
 }
